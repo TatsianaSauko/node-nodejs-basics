95d0ba9df168303e996812bab07e4ebcf89f3836
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:51:01 +0300

    feat: add list functiony

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,31 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const list = async () => {
-    // Write your code here 
+    const directoryPath = path.join(__dirname, 'files');
+    try {
+        await fs.access(directoryPath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed: files does not exist');
+            return;
+        } else {
+            throw error;
+        }
+    }
+
+    try {
+        const files = await fs.readdir(directoryPath);
+        for (const file of files) {
+            const srcFile = path.join(directoryPath, file);
+            console.log(path.basename(srcFile));        }
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 await list();
