4901c80d4b05872094d4b0533431e80ae6c07bcb
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:55:08 +0300

    feat: add read functiony

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const readStream = fs.createReadStream(filePath);
+
+    readStream.on('data', function(chunk) {
+        process.stdout.write(chunk);
+    });
+
+    readStream.on('error', function(err) {
+        console.error('An error has occurred.', err);
+    });
+
 };
 
 await read();
