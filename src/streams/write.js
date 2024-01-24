20beaa6787614d41c8980982db861b50bc3823a7
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:55:41 +0300

    feat: add write functiony

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+    const writeStream = fs.createWriteStream(filePath);
+
+    process.stdin.on('data', function(chunk) {
+        writeStream.write(chunk);
+    });
+
+    process.stdin.on('end', function() {
+        writeStream.end();
+    });
+
+    writeStream.on('error', function(err) {
+        console.error('An error has occurred.', err);
+    });
 };
 
 await write();
