e783b2c4dc21c320e30500d50cbbca4c058b0735
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:51:37 +0300

    feat: add read functiony

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,27 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    try {
+        await fs.access(filePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed: fileToRead.txt does not exist');
+            return;
+        } else {
+            throw error;
+        }
+    }
+    try {
+        console.log((await fs.readFile(filePath)).toString())
+    } catch (error) {
+        console.error('Failed to read file:', error)
+    }
 };
 
 await read();
