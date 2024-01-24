7e5c2e5f44831648fbf61cc4854b4fa9fff6b871
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:57:20 +0300

    feat: add decompress functiony

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import zlib from 'zlib';
+import { pipeline } from 'stream/promises';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const filePathArchive = path.join(__dirname, 'files', 'archive.gz');
+    const filePathFileToCompress = path.join(__dirname, 'files', 'fileToCompress.txt');
+
+    try {
+        await fs.promises.access(filePathArchive);
+    } catch (err) {
+        console.error('File archive.gz does not exist.');
+        return;
+    }
+
+    await pipeline(
+        fs.createReadStream(filePathArchive),
+        zlib.createGunzip(),
+        fs.createWriteStream(filePathFileToCompress),
+    );
 };
 
 await decompress();
