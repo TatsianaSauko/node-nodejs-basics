b9cd37d54ebb1e7f56f5146ce95d0542a0aec727
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:48:33 +0300

    feat: add copy function to create files_copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,46 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const copy = async () => {
-    // Write your code here 
+    const directoryPathFiles = path.join(__dirname, 'files');
+    const directoryPathFiles_copy = path.join(__dirname, 'files_copy');
+
+    try {
+        await fs.access(directoryPathFiles_copy);
+        throw new Error('FS operation failed: files_copy already exists');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+    }
+
+    try {
+        await fs.access(directoryPathFiles);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed: source directory does not exist');
+            return;
+        } else {
+            throw error;
+        }
+    }
+
+    try {
+        await fs.mkdir(directoryPathFiles_copy);
+    
+        const files = await fs.readdir(directoryPathFiles);
+        for (const file of files) {
+            const srcFile = path.join(directoryPathFiles, file);
+            const destFile = path.join(directoryPathFiles_copy, file);
+            await fs.copyFile(srcFile, destFile);
+        }
+    } catch (error) {
+        console.error('Failed to copy files:', error);
+    }
 };
 
 await copy();
