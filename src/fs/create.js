1bfc92dff13591286750ab509c942381acaaa520
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:47:52 +0300

    feat: add create function to create fresh.txt file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
+  try {
+    await fs.access(filePath);
+    throw new Error('FS operation failed: file already exists');
+  } catch (error) {
+    if (error.code === 'ENOENT') {
+      await fs.writeFile(filePath, 'I am fresh and young');
+    } else {
+      throw error;
+    }
+  }
 };
 
 await create();
