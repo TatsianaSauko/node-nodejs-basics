8671c55d9e8fb04a5086e821d9fdb8bdb330aef8
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:49:15 +0300

    feat: add rename functiony

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,38 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const properFilePath = path.join(__dirname, 'files', 'properFilename.md');
+    const wrongFilePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+    try {
+        await fs.access(properFilePath);
+        console.error('FS operation failed: properFilename.md already exists');
+        return;
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+    }
+    try {
+        await fs.access(wrongFilePath);
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed: wrongFilename.txt does not exist');
+            return;
+        } else {
+            throw error;
+        }
+    }
+    try {
+        await fs.rename(wrongFilePath, properFilePath);
+    } catch (error) {
+       console.error(error)
+    }
+
 };
 
 await rename();
