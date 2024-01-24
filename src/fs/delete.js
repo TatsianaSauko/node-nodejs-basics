08d4d1a266317824a1e1b28f4b4b52875b131eb5
TatsianaSauko <miljanzowa@mail.ru>
2024-01-24 12:50:21 +0300

    feat: add remove functiony

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,24 @@
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, 'files', 'fileToRemove.txt');
+    try {
+        await fs.access(filePath);
+        await fs.unlink(filePath);
+
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            console.error('FS operation failed: fileToRemove.txt does not exist');
+            return;
+        } else {
+            throw error;
+        }
+    }
 };
 
 await remove();
