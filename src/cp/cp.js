82d66c222cf308740d2387c2dc910e35cefe9fe9
TatsianaSauko <miljanzowa@mail.ru>
2024-01-25 13:29:43 +0300

    feat: add spawnChildProcess functiony

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,25 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [filePath, ...args], { stdio: ['pipe', 'pipe', 'inherit', 'ipc'] });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
+
+    child.on('error', (error) => {
+        console.error(`Error: ${error.message}`);
+    });
+
+    child.on('exit', (code, signal) => {
+        if (code) console.log(`Process exited with code ${code}`);
+        if (signal) console.log(`Process was killed with signal ${signal}`);
+    });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['arg1', 'arg2', 'arg3']);
