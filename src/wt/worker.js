53e2b412033f86c3a9bd1a0c9d1025538bf865e6
TatsianaSauko <miljanzowa@mail.ru>
2024-01-26 12:55:18 +0300

    feat: add createWorker functiony

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,10 +1,13 @@
-import { parentPort } from 'worker_threads';
+import { parentPort, workerData  } from 'worker_threads';
 
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    const result = nthFibonacci(n);
-    parentPort.postMessage(result);
+    const { number } = workerData;
+
+    const result = nthFibonacci(number);
+
+    parentPort.postMessage({ status: 'resolved', data: result });
 };
 
 sendResult();
